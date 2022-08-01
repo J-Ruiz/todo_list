0e6a6c589fe31efd8033cb3cb12189de2a608749
Jake R <jwr318@gmail.com>
2022-08-01 16:44:12 -0400

    created the logic to add tasks to the appropriate tabs. Upon clicking each tab, it will evaluate the task according to todays date. the functionality to clear the tasks and recreate the dom elements on each click will allow the tasks to continually update if a task is past the current date, into the appropriate -past- tab. When a task is submitted it generates a dom element and sorts it into the appropriate tab. The next updates will incorporate validation for the date and title of the task as well as css styling upon the click of the checkbox to signify that the task has been complete. Additional updates will need to add new projects tabs to the projects menu div.

--- src/index.js
+++ src/index.js
@@ -1,72 +1,113 @@
 import './style.css';
 import { compareAsc } from 'date-fns'
 
-let tasks = [];
-let currentWindow = document.getElementById("all-content");
-let todaysDate = new Date().toISOString().slice(0,10);
+
+const activeWindowsAndTaskStorage ={
+    currentWindow: "all-content-tasks",
+    previousWindow: "",
+    tasks: []
+};
+
+const displayContentWindows = {
+    "all" : "all-content-tasks",
+    "past": "past-content-tasks",
+    "today": "today-content-tasks",
+    "upcoming": "upcoming-content-tasks",
+    "trigger-add-task-popup": "popup-add-task",
+    "trigger-add-project-popup": "popup-add-project"
+};
+
+const displayTitles = {
+    "all" : "All Tasks",
+    "past": "Past Tasks",
+    "today": "Today's Tasks",
+    "upcoming": "Upcoming Tasks",
+    "trigger-add-task-popup": "Create A New Task",
+    "trigger-add-project-popup": "Create a New Project",
+    "all-content-tasks": "All Tasks",
+    "past-content-tasks": "Past Tasks",
+    "today-content-tasks": "Today's Tasks",
+    "upcoming-content-tasks": "Upcoming Tasks"
+};
 
 
+function displaySideBarTabs(){
+    let sideBarTabs = Array.from(document.getElementsByClassName("side-bar-tabs"));
+    sideBarTabs.forEach(tab => tab.addEventListener("click", (event)=>{
+        displayTabs(event.target.id);
 
-/* Clear tasks 
-        - Get current window from the currentWindow variable
-        - Clear all tasks from popup content window
-*/
-
-function clearTasks(currentWindow){
-    let window = document.getElementById(`${currentWindow}`)
-        window.innerHTML = "";
-    return;
+    }));
 }
 
 
-/* Hide Windows
-        -take current window and change the visiblity : "Hidden";
-        -take the clicked window and change visibility : "visible"
-*/
-
-let sideBarTabs = document.getElementsByClassName(side-bar-tabs);
-
-sideBarTabs.forEach(tab => tab.addEventListener("click", (event)=>{
-    windowFocus(event.target.id);
-}))
-function windowFocus(event){
-    clearTasks(currentWindow);
-    document.getElementById(currentWindow).style.visibility = "hidden";
-    currentWindow = event.target.id + "-content";
-    document.getElementById(currentWindow).style.visibility = "visible";
-    return;
-}
-
-
-// make pop up window visible
-let addTaskPopup = document.getElementById("popup-add-task");
-    addTaskPopup.addEventListener("click",()=>{
-        currentWindow.style.visibility = "hidden";
-        currentWindow = "popup-add-task";
-        addTaskPopup.style.visibility = "visible";
-        return;
-    })
-
-/* New Task Object 
-        - Create factory function or class to make a new Task object 
-            title: get value from task-title input
-            date: get date from task-date input 
-            status: "not done"
-
-        - push task into the tasks variable
+function displayTabs(id){
+        document.getElementById("window-title").innerHTML = displayTitles[id]
+        document.getElementById(activeWindowsAndTaskStorage.currentWindow).style.visibility = "hidden";
+        activeWindowsAndTaskStorage.currentWindow = displayContentWindows[id];
+        document.getElementById(activeWindowsAndTaskStorage.currentWindow).style.visibility = "visible";
         
-*/
+        if(activeWindowsAndTaskStorage.currentWindow == "popup-add-task" || 
+           activeWindowsAndTaskStorage.currentWindow == "popup-add-project"){     
+            return;
+        }
 
-function addTask(){
-    let addTaskButton = document.getElementById("add-task");
-        addTaskButton.addEventListener("click", ()=>{
-            let title = document.getElementById("task-title").value;
-            let date = document.getElementById("task-date").value;
-            let status = "not done";
-            let newTask = newTaskObject(title, date, status);
-            tasks.push(newTask);
-        })
-}  
+        else{
+            clearTasks(activeWindowsAndTaskStorage.currentWindow);
+            displayTasks(filterTasks(activeWindowsAndTaskStorage.tasks), id);
+            return;
+        }
+    
+};
+
+
+function displayAddTaskPopup(){
+    document.getElementById("trigger-add-task-popup").addEventListener("click", (event)=>{
+        displayTabs(event.target.id);
+    })
+}
+
+function displayAddProjectPopup(){
+    document.getElementById("trigger-add-project-popup").addEventListener("click", (event)=>{
+        displayTabs(event.target.id);
+    })
+};
+
+function submitAddTaskPopup(){
+    document.getElementById("add-task-to-dom").addEventListener("click", ()=>{
+        let storeUserInput = pullTaskUserInput();
+        let createTaskObject = newTaskObject(storeUserInput.title, storeUserInput.date, storeUserInput.status);
+        let filteredTasks = filterTasks(activeWindowsAndTaskStorage.tasks);
+        clearTasks("all-content-tasks")
+        displayTasks(filteredTasks, "all")
+        hideProjectsAndTasks();
+    })
+};
+
+function submitAddProjectPopup(){
+    document.getElementById("add-project-to-dom").addEventListener("click", ()=>{
+        hideProjectsAndTasks();
+        //might want to make a separate function HIDE PROJECTS that immediately opens the new project
+        //allowing user to add new tasks
+    })
+};
+
+
+function hideProjectsAndTasks(){
+        document.getElementById(activeWindowsAndTaskStorage.currentWindow).style.visibility = "hidden";
+        activeWindowsAndTaskStorage.currentWindow = "all-content-tasks";
+        document.getElementById("window-title").innerHTML = displayTitles["all"];
+        document.getElementById(activeWindowsAndTaskStorage.currentWindow).style.visibility = "visible";
+};
+
+
+
+
+
+displaySideBarTabs();
+displayAddTaskPopup();
+submitAddTaskPopup();
+displayAddProjectPopup();
+submitAddProjectPopup();
 
 function newTaskObject(title,date,status){
     const taskFactory = (title, date, status) => {
@@ -74,152 +115,135 @@ function newTaskObject(title,date,status){
     }
     let newTask= taskFactory(title,date,status);
 
+    activeWindowsAndTaskStorage.tasks= activeWindowsAndTaskStorage.tasks.concat(newTask);
+    
     return newTask
+
+};
+
+function pullTaskUserInput(){
+    let title = document.getElementById("task-title").value; 
+    let date = document.getElementById("task-date").value;
+    let status = "not done";
+
+    return {title, date, status};
+};
+
+/*
+    1. hit add task
+    2. take me back to all tasks window
+    3. add tasks to all tasks
+    4. if all tasks contains tasks, then we will create filter as we go
+        recreating the dom objects to ensure they are up to date 
+
+*/
+
+function displayTasks(filteredTasks, id){
+    if(id == "all"){
+
+        for(let i = 0; i<filteredTasks.allTasks.length; i++){
+            let domTask = makeTaskDomElement(filteredTasks.allTasks[i]);
+            document.getElementById("all-content-tasks").append(domTask);
+        }
+        return;
+    }
+
+    else if (id == "past"){
+        for(let i = 0; i<filteredTasks.pastTasks.length; i++){
+            let domTask = makeTaskDomElement(filteredTasks.pastTasks[i]);
+            document.getElementById("past-content-tasks").append(domTask);
+        }
+        return;
+    }
+
+    else if(id == "today"){
+        for(let i = 0; i<filteredTasks.todaysTasks.length; i++){
+            let domTask = makeTaskDomElement(filteredTasks.todaysTasks[i]);
+            document.getElementById("today-content-tasks").append(domTask);
+        }
+        return;
+     }
+
+    else if(id == "upcoming"){
+        for(let i = 0; i<filteredTasks.upcomingTasks.length; i++){
+            let domTask = makeTaskDomElement(filteredTasks.upcomingTasks[i]);
+            document.getElementById("upcoming-content-tasks").append(domTask);
+        }
+        return;
+    }
+    else{
+        return;
+    }
 }
 
-function filterTasks(array){
-    let filteredTasks = tasks.filter(task=>{
-        
-    })
-}
-
-function makeTaskDomElement(tasks){
+function makeTaskDomElement(task){
+    
 
     let newTask = document.createElement("div");
+        newTask.setAttribute("class", "created-tasks");
 
-        let checkBox = document.createElement("input")
-            checkBox.setAttribute("type", "checkbox")
-            checkBox.addEventListener("click", finishTask)
+        let checkboxAndTitle = document.createElement("div");
+            checkboxAndTitle.setAttribute("class", "checkbox-and-title")
+            
+        let checkbox = document.createElement("input")
+            checkbox.setAttribute("type", "checkbox")
+            //checkBox.addEventListener("click", finishTask)
 
         let title = document.createElement("div");
             title.innerHTML = task.title;
 
+        checkboxAndTitle.append(checkbox, title)
+
         let date = document.createElement("div");
             date.innerHTML = task.date;
 
-        newTask.append(checkBox, title, date)
+        let status = document.createElement("div");
+            status.innerHTML = task.status;
+
+        newTask.append(checkboxAndTitle, date)
 
     return newTask;
 
 }
 
-  
+function filterTasks(tasks){
+    let allTasks = tasks;
+    let pastTasks =  tasks.filter(task => compareDates(task.date) == -1); 
+    let todaysTasks = tasks.filter(task => compareDates(task.date) == 0); 
+    let upcomingTasks = tasks.filter(task => compareDates(task.date) == 1); 
 
-
-/* 2. New Project Tab (takes one argument, the name of the project)
-        - Create dom element project tab 
-        - append new project tab to task-side-menu-2
-*/
-
-function newProject(projectName){
-    let newProjectTab = document.createElement("div")
-        newProjectTab.setAttribute("id", `${projectName}`)
-    
-    let projectMenu = document.getElementById("task-side-menu-2");
-        projectMenu.appendChild(newProjectTab)
-
-    return; 
+    return {allTasks, pastTasks, todaysTasks, upcomingTasks}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* 5. Finishing a task
-            : update status to "done"
-            : re-style the task and have a line put through it
-            : make the lettering gray 
-        
-*/
-
-function finishTask(task){
-        /*find index of the passed in task in the task global variable 
-          target index in array and ...
-            : update status to "done"
-            : Target dom element and re-style the task and have a line put through it
-            : make the lettering gray*/
-         
-}
-
-
-/* 6. Tab event listeners 
-        - add event listeners on click for every tab 
-        - if clicked, **RUN THE CLEAR TASKS FUNCTION** on current content box 
-        - **RUN THE APPEND TASKS TO DOM FUNCTION** passing an argument that identifies the tab clicked 
-
-*/
-
-function tabListeners(){
-    let tabs = document.getElementsByClassName("side-bar-tabs");
-
-    for(let i = 0; i<tabs.length; i++){
-        tabs[i].addEventListener("click", ()=>{
-            clearTasks();
-            //append tasks for the appropriately clicked window 
-        })
-    }
-}
-
-
-/* 7. Make Task DOM Element 
-        - for loop iterating over current loop 
-        - create a new dom element with title and date of task
-        - add a completion check box to dom element (**EVENT LISTENER FINISH A TASK FUNC**)
-*/ 
-
-
-
-
-/* 8. COMPARE DATES
-
-        - **CREATED GLOBAL VARIABLE CALLED TODAYSDATE
-            let today = new Date().toISOString().slice(0, 10) 
-            above grabs the date and slices the first 10 characters off which is in the same format
-            that the date input will store the "value" of the date 
-
-        - compare dates using date-fns recommended in odin project
-            import { compareAsc } from 'date-fns'
-            compareAsc(dateLeft, dateRight) returns 1 if first is after, 0 if equal -1 if before 
-            dateright should be todays date
-            dateleft is whatever is the value inputted by the user 
-        
-
-*/
 
 function compareDates(userDate){
 
-    let userYear = userDate.slice(0,4);
-    let userMonth = userDate.slice(5,7);
-    let userDay = userDate.slice(8);
-    let userDateCopy = newDate(userYear, userMonth, userDay);
+        let userYear = userDate.slice(0,4);
+        let userMonth = userDate.slice(5,7);
+        let userDay = userDate.slice(8);
 
-    let todaysYear = todaysDate.slice(0,4);
-    let todaysMonth = todaysDate.slice(5,7);
-    let todaysDay = todaysDate.slice(8);
-    let todaysDateCopy = newDate(todaysYear, todaysMonth, todaysDay)
+    let userDateCopy = new Date(userYear, userMonth, userDay);
+
+        let todaysDate = new Date().toISOString().slice(0,10);
+        let todaysYear = todaysDate.slice(0,4);
+        let todaysMonth = todaysDate.slice(5,7);
+        let todaysDay = todaysDate.slice(8);
+
+    let todaysDateCopy = new Date(todaysYear, todaysMonth, todaysDay)
 
     let designation = compareAsc(userDateCopy, todaysDateCopy);
 
-    if(designation == -1){
-        return "past"
-    }
+    return designation;
 
-    else if (designation == 0){
-        return "today"
-    }
+};
 
-    else {
-        return "upcoming"
-    }
 
-    return; 
+function clearTasks(id){
+    document.getElementById(id).innerHTML = "";
+    return;
 }
+
+
+
+
 
